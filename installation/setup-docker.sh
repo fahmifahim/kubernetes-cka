e9d690648fd27a1055705b07e4f64abae90dca4b
Dzulfahmi <fahmifahim@gmail.com>
2020-05-06 15:26:21 +0900

    Create setup-docker.sh

---
+++ installation/setup-docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# script that runs 
+# https://kubernetes.io/docs/setup/production-environment/container-runtime
+
+yum install -y vim yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# notice that only verified versions of Docker may be installed
+# verify the documentation to check if a more recent version is available
+
+yum install -y docker-ce
+[ ! -d /etc/docker ] && mkdir /etc/docker
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+cat >> /etc/hosts << EOF
+{
+    192.168.11.14	master.example.com control master
+    192.168.11.15	worker1.example.com worker1
+    192.168.11.16	worker2.example.com worker2
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+systemctl disable --now firewalld
